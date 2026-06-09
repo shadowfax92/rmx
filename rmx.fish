c8b3bb553efa0665f712d645c7d085a6d4ef4791
Nikhil Sonti <nikhilsv92@gmail.com>
2026-06-08 18:43:01 -0700

    feat: add rmx fish helper for wrapux

---
+++ rmx.fish
@@ -0,0 +1,21 @@
+function rmx --description 'fzf-powered rmux session wrapper (wrapux)'
+    if test (count $argv) -eq 0
+        wrapux ls
+        return
+    end
+
+    set -l rest $argv[2..]
+
+    switch $argv[1]
+        case l ls list
+            wrapux ls $rest
+        case a attach
+            wrapux attach $rest
+        case c cap capture
+            wrapux capture $rest
+        case rm k kill remove
+            wrapux rm $rest
+        case '*'
+            wrapux $argv
+    end
+end
